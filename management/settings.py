8a073edfba2f0af5b0a2470c04a8266d03598abe
Swayam <swayamsiddhap@gmail.com>
2023-03-28 18:07:33 +0530

    Edited new

--- management/settings.py
+++ management/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    #Apps
+    "debug_toolbar",
+    
 ]
 
 MIDDLEWARE = [
@@ -47,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "management.urls"
